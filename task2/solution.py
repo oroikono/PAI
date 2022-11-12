8c564ea5656d87bf7877b3389fce44fee5aa56bb
oroikono <orestis.oikonomou@uzh.ch>
2022-11-12 18:54:13 +0100

    epochs = 100, batch size = 64

--- task2/solution.py
+++ task2/solution.py
@@ -350,7 +350,7 @@ class DropoutTrainer(Framework):
 
                 current_logits = self.network(batch_x)
                 # current_logits = torch.nn.functional.softmax(current_logits)
-                tau = 0.01
+                tau = 1
                 l2_norm = sum(p.pow(2.0).sum() for p in self.network.parameters())
                 loss = nn.CrossEntropyLoss()(current_logits, batch_y)+tau*l2_norm
 
